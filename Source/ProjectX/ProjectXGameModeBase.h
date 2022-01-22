7d0c115752eb6bd1d19b03a581ec39093e4ce666
SiCoYu <yxk_neu2013@163.com>
2022-01-22 23:14:33 +0800

    add 4.27 android pak demo

---
+++ Source/ProjectX/ProjectXGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectXGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTX_API AProjectXGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
